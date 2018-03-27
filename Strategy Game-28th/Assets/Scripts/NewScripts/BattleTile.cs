d5271f885b030b18b5573e53bc68b403d47a9352
Unknown <adeptusforgeceo@gmail.com>
2018-03-27 06:04:14 -0500

    Script Divergence & Repo Fix

---
+++ Strategy Game-28th/Assets/Scripts/NewScripts/BattleTile.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+[CreateAssetMenu(menuName = "BattleTile")]
+public class BattleTile : Tile
+{
+    public BattleTileInfo info;
+    public bool occupied;
+    public Entity occupiedBy;
+
+
+
+    public BattleTile(BattleTileInfo _info)
+    {
+        info = _info;
+
+    }
+}
---
+++ Strategy Game-28th/Assets/Scripts/NewScripts/Constants.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Constants {
+
+    public enum Tiletype
+    {
+
+    }
+
+    public enum Faction
+    {
+
+    }
+
+    public enum TileModifier
+    {
+
+    }
+
+    public enum UnitType
+    {
+
+    }
+
+    public enum UnitTraits
+    {
+
+    }
+
+    public enum AttackModifier
+    {
+
+    }
+}
